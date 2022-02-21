9ddd23315f07402a0ae1e77f0bd248e9525a6f00
MrKruten <80253384+MrKruten@users.noreply.github.com>
2022-02-21 20:38:39 +0700

    refactor: api currency

--- src/shared/api/currency.ts
+++ src/shared/api/currency.ts
@@ -22,35 +22,41 @@ export interface IQuery {
 	timestamp: number;
 }
 
-const getCurrency = async (
-	currency: string,
-	dateFrom?: string,
-	dateTo?: string
+const getCurrencyLatest = async (
+	currency: string
 ): Promise<IResponseCurrency> => {
-	const url = dateFrom
-		? `${API_URL}historical?apikey=${API_KEY}&base_currency=${currency}&date_from=${dateFrom}&date_to=${dateTo}`
-		: `${API_URL}latest?apikey=${API_KEY}&base_currency=${currency}`;
+	const url = `${API_URL}latest?apikey=${API_KEY}&base_currency=${currency}`;
+	const response = await fetch(url);
+	return await response.json();
+};
+
+const getCurrencyHistorical = async (
+	currency: string,
+	dateFrom: string,
+	dateTo: string
+): Promise<IResponseHistoricalCurrency> => {
+	const url = `${API_URL}historical?apikey=${API_KEY}&base_currency=${currency}&date_from=${dateFrom}&date_to=${dateTo}`;
 	const response = await fetch(url);
 	return await response.json();
 };
 
 export const getCurrencyToday = async (currency: string) =>
-	await getCurrency(currency);
+	await getCurrencyLatest(currency);
 
 export const getCurrencyWeek = async (currency: string) => {
-	const dateFrom = getDateToday();
-	const dateTo = getDateWeek();
-	return await getCurrency(currency, dateFrom, dateTo);
+	const dateTo = getDateToday();
+	const dateFrom = getDateWeek();
+	return await getCurrencyHistorical(currency, dateFrom, dateTo);
 };
 
 export const getCurrencyMonth = async (currency: string) => {
-	const dateFrom = getDateToday();
-	const dateTo = getDateMonth();
-	return await getCurrency(currency, dateFrom, dateTo);
+	const dateTo = getDateToday();
+	const dateFrom = getDateMonth();
+	return await getCurrencyHistorical(currency, dateFrom, dateTo);
 };
 
 export const getCurrencyYear = async (currency: string) => {
-	const dateFrom = getDateToday();
-	const dateTo = getDateYear();
-	return await getCurrency(currency, dateFrom, dateTo);
+	const dateTo = getDateToday();
+	const dateFrom = getDateYear();
+	return await getCurrencyHistorical(currency, dateFrom, dateTo);
 };
